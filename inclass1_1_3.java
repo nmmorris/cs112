06b1dfea7dc6cc07d023a49081060862cce696e8
nmmorris <54457700+nmmorris@users.noreply.github.com>
2019-08-26 17:36:19 -0700

    Add files via upload

--- inclass1_1_3.java
+++ inclass1_1_3.java
@@ -7,16 +7,18 @@ public class inclass1_1_3
 		Scanner Input=new Scanner(System.in);
 		double first_variable;
 		double second_variable;
-		double new_first_variable, new_second_variable;
+		double temp_first_variable, temp_second_variable;
 		System.out.print("Enter value for first variable: ");
 		first_variable=Input.nextDouble();
-		new_second_variable=first_variable;
 		System.out.print("Enter value for second variable: ");
 		second_variable=Input.nextDouble();
-		new_first_variable=second_variable;
 		System.out.println("Original first variable: " + first_variable);
 		System.out.println("Original second variable: " + second_variable);
-		System.out.println("New first variable: " + new_first_variable);
-		System.out.println("New second_variable: " + new_second_variable);
+		temp_first_variable=first_variable;
+		temp_second_variable=second_variable;
+		first_variable=temp_second_variable;
+		second_variable=temp_first_variable;
+		System.out.println("New first variable: " + first_variable);
+		System.out.println("New second variable: " + second_variable);
 	}
 }
